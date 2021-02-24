26d2039c0cbf58dd93bebfd2de0f245d04598ce0
ben_kung <ben_kung@weleader.com.tw>
2021-02-24 12:00:34 +0800

    aa

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-import fire
-import os
+# import fire
+# import os
 
-class Generator(object):
-  ''' 產生flutter provider mvvm範本程式 '''
-  def full_code(self, name):
-    ''' 產生完整範本程式 '''
-    # models
-    if not os.path.exists('models'):
-        os.makedirs('models')    
-    open(f"models/{str(name).lower()}.dart", "x")
+# class Generator(object):
+#   ''' 產生flutter provider mvvm範本程式 '''
+#   def full_code(self, name):
+#     ''' 產生完整範本程式 '''
+#     # models
+#     if not os.path.exists('models'):
+#         os.makedirs('models')    
+#     open(f"models/{str(name).lower()}.dart", "x")
 
-    # view_models
-    if not os.path.exists('view_models'):
-        os.makedirs('view_models')     
-    open(f"view_models/{str(name).lower()}_view_model.dart", "x")
+#     # view_models
+#     if not os.path.exists('view_models'):
+#         os.makedirs('view_models')     
+#     open(f"view_models/{str(name).lower()}_view_model.dart", "x")
 
-    # services
-    if not os.path.exists('services'):
-        os.makedirs('services')      
-    open(f"services/{str(name).lower()}_service.dart", "x")
+#     # services
+#     if not os.path.exists('services'):
+#         os.makedirs('services')      
+#     open(f"services/{str(name).lower()}_service.dart", "x")
 
-    # pages
-    if not os.path.exists('pages'):
-        os.makedirs('pages')      
-    open(f"pages/{str(name).lower()}_page.dart", "x")
+#     # pages
+#     if not os.path.exists('pages'):
+#         os.makedirs('pages')      
+#     open(f"pages/{str(name).lower()}_page.dart", "x")
 
-    return name
+#     return name
 
-if __name__ == '__main__':
-  fire.Fire(Generator)
+# if __name__ == '__main__':
+#   fire.Fire(Generator)
+print('fuck')
