e66d977c52c03207f09d543b6ba9cd32057f4df1
Ben <moon791017@gmail.com>
2021-02-24 10:43:57 +0800

    aa

---
+++ main.py
@@ -0,0 +1,14 @@
+import fire
+
+class Generator(object):
+  ''' 產生flutter provider mvvm範本程式 '''
+  def full_code(self, name):
+    ''' 產生完整範本程式 '''
+    # models
+    # view_models
+    # services
+    # pages
+    return name
+
+if __name__ == '__main__':
+  fire.Fire(Generator)
