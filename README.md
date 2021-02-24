1e38bea9d537467473d739b64126d1f1e5d510d0
ben_kung <ben_kung@weleader.com.tw>
2021-02-24 14:21:39 +0800

    AAA

---
+++ README.md
@@ -0,0 +1,8 @@
+``` bash
+# 安裝package
+python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+
+# 打包程式
+pyinstaller --onefile   main.py
+```
--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-# import fire
-# import os
+import fire
+import os
 
-# class Generator(object):
-#   ''' 產生flutter provider mvvm範本程式 '''
-#   def full_code(self, name):
-#     ''' 產生完整範本程式 '''
-#     # models
-#     if not os.path.exists('models'):
-#         os.makedirs('models')    
-#     open(f"models/{str(name).lower()}.dart", "x")
+class Generator(object):
+  ''' 產生flutter provider mvvm範本程式 '''
+  def full_code(self, file_name,app_name):
+    ''' 產生完整範本程式 '''
+    # models
+    if not os.path.exists('models'):
+        os.makedirs('models')    
+    model_file=open(f"models/{str(file_name).lower()}.dart", "x")
+    model_file.write(f'''class {str(file_name).capitalize ()} {{
+            {str(file_name).capitalize ()}();
+        }}''')
+    model_file.close()
+    # view_models
+    if not os.path.exists('view_models'):
+        os.makedirs('view_models')     
+    open(f"view_models/{str(file_name).lower()}_view_model.dart", "x")
 
-#     # view_models
-#     if not os.path.exists('view_models'):
-#         os.makedirs('view_models')     
-#     open(f"view_models/{str(name).lower()}_view_model.dart", "x")
+    # services
+    if not os.path.exists('services'):
+        os.makedirs('services')      
+    open(f"services/{str(file_name).lower()}_service.dart", "x")
 
-#     # services
-#     if not os.path.exists('services'):
-#         os.makedirs('services')      
-#     open(f"services/{str(name).lower()}_service.dart", "x")
+    # pages
+    if not os.path.exists('pages'):
+        os.makedirs('pages')      
+    open(f"pages/{str(file_name).lower()}_page.dart", "x")
 
-#     # pages
-#     if not os.path.exists('pages'):
-#         os.makedirs('pages')      
-#     open(f"pages/{str(name).lower()}_page.dart", "x")
+    return file_name
 
-#     return name
-
-# if __name__ == '__main__':
-#   fire.Fire(Generator)
-print('fuck')
+if __name__ == '__main__':
+  fire.Fire(Generator)
