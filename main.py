b997967591df493548f472c17174b23740439553
ben <moon791017@gmail.com>
2021-03-25 11:38:39 +0800

    update

--- main.py
+++ main.py
@@ -4,30 +4,32 @@ import subprocess
 from pathlib import Path
 from jinja2 import Environment, PackageLoader, FileSystemLoader, Template
 import requests
+import re
 root_template_url = 'https://raw.githubusercontent.com/Benknightdark/provider_scaffold_cli/main/templates'
 
 
 def generate_code(template_type, file_name, app_name):
     '''產生範本程式 '''
+    split_file_name='_'.join(re.findall('[A-Z][a-z]*', file_name)) 
     template_url = f'{root_template_url}/{template_type}'
     r = requests.get(f'{template_url}/model.jinja').text
-    model_template_output = Template(r).render(file_name=file_name)
+    model_template_output = Template(r).render(file_name=file_name,split_file_name=split_file_name)
 
     r = requests.get(f'{template_url}/view_model.jinja').text
     view_model_template_output = Template(r).render(
-        app_name=app_name, file_name=file_name)
+        app_name=app_name, file_name=file_name,split_file_name=split_file_name)
 
     r = requests.get(f'{template_url}/view.jinja').text
     view_template_output = Template(r).render(
-        app_name=app_name, file_name=file_name)
+        app_name=app_name, file_name=file_name,split_file_name=split_file_name)
 
     r = requests.get(f'{template_url}/service.jinja').text
-    service_template_output = Template(r).render(file_name=file_name)
+    service_template_output = Template(r).render(file_name=file_name,split_file_name=split_file_name)
 
     # models
     if not os.path.exists('lib/models'):
         os.makedirs('lib/models')
-    model_file = open(f"lib/models/{str(file_name).lower()}.dart", "w+")
+    model_file = open(f"lib/models/{str(split_file_name).lower()}_model.dart", "w+")
     model_file.write(model_template_output)
     model_file.close()
 
@@ -35,7 +37,7 @@ def generate_code(template_type, file_name, app_name):
     if not os.path.exists('lib/view_models'):
         os.makedirs('lib/view_models')
     view_model_file = open(
-        f"lib/view_models/{str(file_name).lower()}_view_model.dart", "w+")
+        f"lib/view_models/{str(split_file_name).lower()}_view_model.dart", "w+")
     view_model_file.write(view_model_template_output)
     view_model_file.close()
 
@@ -43,13 +45,13 @@ def generate_code(template_type, file_name, app_name):
     if not os.path.exists('lib/services/api'):
         os.makedirs('lib/services/api')
     service_file = open(
-        f"lib/services/api/{str(file_name).lower()}_service.dart", "w+")
+        f"lib/services/api/{str(split_file_name).lower()}_service.dart", "w+")
     service_file.write(service_template_output)
     service_file.close()
     # pages
     if not os.path.exists('lib/pages'):
         os.makedirs('lib/pages')
-    page_file = open(f"lib/pages/{str(file_name).lower()}_page.dart", "w+")
+    page_file = open(f"lib/pages/{str(split_file_name).lower()}_page.dart", "w+")
     page_file.write(view_template_output)
     page_file.close()
     os.system(
