0d226cab78eab1b1bd0a59b5e1c44ed05cd05803
ben <moon791017@gmail.com>
2021-04-28 15:30:13 +0800

    update jinja template

--- main.py
+++ main.py
@@ -12,24 +12,30 @@ root_template_url = 'https://raw.githubusercontent.com/Benknightdark/provider_sc
 
 def generate_code(template_type, file_name, app_name):
     '''產生範本程式 '''
-    split_file_name='_'.join(re.findall('[A-Z][a-z]*', file_name))
+    split_array=(re.findall('[A-Z][a-z]*', file_name))
+    if len(split_array)>0:
+        split_file_name='_'.join(re.findall('[A-Z][a-z]*', file_name))
+    else:
+        split_file_name=file_name
     camel_file_name=humps.camelize(split_file_name)
-    print(camel_file_name)
-    print(split_file_name) 
+    class_name=split_file_name.replace('_', '')
+    print(split_file_name)
+    print(camel_file_name) 
+    print(class_name)
     template_url = f'{root_template_url}/{template_type}'
     r = requests.get(f'{template_url}/model.jinja').text
-    model_template_output = Template(r).render(file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name)
+    model_template_output = Template(r).render(file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name,class_name=class_name)
 
     r = requests.get(f'{template_url}/view_model.jinja').text
     view_model_template_output = Template(r).render(
-        app_name=app_name, file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name)
+        app_name=app_name, file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name,class_name=class_name)
 
     r = requests.get(f'{template_url}/view.jinja').text
     view_template_output = Template(r).render(
-        app_name=app_name, file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name)
+        app_name=app_name, file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name,class_name=class_name)
 
     r = requests.get(f'{template_url}/service.jinja').text
-    service_template_output = Template(r).render(file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name)
+    service_template_output = Template(r).render(file_name=file_name,split_file_name=split_file_name,camel_file_name=camel_file_name,class_name=class_name)
 
     # models
     if not os.path.exists('lib/models'):
